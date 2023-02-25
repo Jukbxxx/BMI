7c4927c89b227106852658a6851ff699d268d1b2
Maria Sarkisyan <mariia.sarkisyan@ya.ru>
2023-02-25 20:53:06 +0300

    Comments added

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
     public int calculate(double m, double kg) {
-        double squaredHeight = Math.pow(m, 2);
-        int result = (int) (kg / squaredHeight);
+        double squaredHeight = Math.pow(m, 2); // Переменная для хранения значения роста в квадрате
+        int result = (int) (kg / squaredHeight); // Делим массу тела в кг на рост в метрах в квадрате, приводим к int
         return result;
     }
 }
