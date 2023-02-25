6d0b646f69c9ffe6af0e83d447722bdba0dc2e67
Maria Sarkisyan <mariia.sarkisyan@ya.ru>
2023-02-25 19:42:36 +0300

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,7 @@
+public class BmiService {
+    public int calculate (double m, double kg) {
+        double squaredHeight = Math.pow(m, 2);
+        int result = (int) (kg / squaredHeight);
+        return result;
+    }
+}
