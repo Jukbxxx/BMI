be20d4049e6c2283fe3272aca10f210533d29b39
Maria Sarkisyan <mariia.sarkisyan@ya.ru>
2023-02-25 20:45:03 +0300

    Code reformated

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public int calculate (double m, double kg) {
+    public int calculate(double m, double kg) {
         double squaredHeight = Math.pow(m, 2);
         int result = (int) (kg / squaredHeight);
         return result;
